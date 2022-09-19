5b20faa5cc65ae936a8239c6405ef5156a1236fb
alanctnk <alanctnk@gmail.com>
2022-09-19 01:13:52 -0300

    start :rocket:

---
+++ main.py
@@ -0,0 +1,132 @@
+#!/usr/bin/python3
+
+import tkinter as tk
+from tkinter import messagebox
+from pwd_module import generate
+import pyperclip
+import json
+
+# ---------------------------- PASSWORD GENERATOR -------------------------- #
+
+
+def generate_pwd():
+    pwd = generate()
+    password_entry.delete(0, tk.END)
+    password_entry.insert(0, pwd)
+    pyperclip.copy(pwd)
+
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def save():
+
+    website = website_entry.get()
+    login = login_entry.get()
+    password = password_entry.get()
+    new_data = {website.lower(): {"login": login, "password": password}}
+
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(
+            title="Oops",
+            message="Please make sure you have not left any fields empty.",
+        )
+    else:
+
+        is_ok = messagebox.askokcancel(
+            title=website,
+            message=f"These are the details entered: \n\nLogin: {login}"
+            + f"\nPassword: {password}\n\nIs it OK to save?",
+        )
+
+        if is_ok:
+            try:
+                with open("data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+
+            else:
+                # Updating old data with new data
+                data.update(new_data)
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+
+            finally:
+                website_entry.delete(0, tk.END)
+                login_entry.delete(0, tk.END)
+                password_entry.delete(0, tk.END)
+
+
+# ---------------------------- FIND PASSWORD --------------------------- #
+def find_pwd():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="No Data File Found.")
+
+    else:
+        if website.lower() in data:
+            appname = website.lower()
+            login = data[appname]["login"]
+            password = data[appname]["password"]
+
+            messagebox.showinfo(
+                title=website,
+                message=f"Login: {login}\nPassword: {password}",
+            )
+        else:
+            messagebox.showinfo(
+                title="Not found", message=f"No details for {website} exists."
+            )
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = tk.Tk()
+window.title("Password Generator")
+window.config(padx=20, pady=20)
+
+canvas = tk.Canvas(width=200, height=200)
+tomato_img = tk.PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=tomato_img)
+canvas.grid(column=1, row=0)
+
+# Labels
+website_label = tk.Label(text="Website:")
+website_label.grid(column=0, row=1)
+login_label = tk.Label(text="Login:")
+login_label.grid(column=0, row=2)
+password_label = tk.Label(text="Password:")
+password_label.grid(column=0, row=3)
+
+# Entries
+website_entry = tk.Entry()
+website_entry.grid(column=1, row=1, sticky="EW")
+website_entry.focus()
+login_entry = tk.Entry()
+login_entry.grid(column=1, row=2, columnspan=2, sticky="EW")
+login_entry.insert(0, "alanctnk@gmail.com")
+password_entry = tk.Entry()
+password_entry.grid(column=1, row=3, sticky="EW")
+
+# Buttons
+search_button = tk.Button(text="Search", width=9, command=find_pwd)
+search_button.grid(row=1, column=2)
+
+generate_button = tk.Button(text="Generate", command=generate_pwd)
+generate_button.grid(column=2, row=3, sticky="EW")
+
+add_button = tk.Button(text="Save credentials", width=35, command=save)
+add_button.grid(column=1, row=4, columnspan=2, sticky="EW")
+
+window.mainloop()
+
+# https://riptutorial.com/tkinter/example/29713/grid--
---
+++ pwd_module.py
@@ -0,0 +1,53 @@
+# Password Generator Project
+import random
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "@", "_", "/", "^"]
+
+
+def generate():
+
+    nr_letters = random.randint(4, 6)
+    nr_symbols = random.randint(1, 2)
+    nr_numbers = random.randint(2, 3)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = (
+        password_letters + password_symbols + password_numbers + ["K"]
+    )
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    return password
